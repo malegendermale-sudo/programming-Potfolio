40be25a9a1718f53a3373c7e9a8d9cb77468d374
malegendermale-sudo <malegendermale@gmail.com>
2025-10-23 21:46:11 +0400

    Add files via upload

---
+++ Exercise 4.py
@@ -0,0 +1,5 @@
+answer = input("What is the cpital of Frnce?")
+if answer.strip().lower() == 'paris':
+    print('Correct! Paris is the capital of france.')
+else:
+    print('Oops! The Answer was Paris')
